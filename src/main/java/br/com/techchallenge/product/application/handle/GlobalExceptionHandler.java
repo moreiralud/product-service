3551c82cb892abd654c7733b95e5bbcb15b6344e
Ludmila Moreira <ludmilamoreira@Victors-MacBook-Air.local>
2025-05-22 16:20:12 -0300

    add handler

---
+++ src/main/java/br/com/techchallenge/product/application/handle/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package br.com.techchallenge.product.application.handle;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
